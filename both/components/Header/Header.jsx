56111048ac974a4bb6b1fba298ce217d96d638a8
Jaf Crisologo <rtql8d@gmail.com>
2016-09-28 09:31:48 -0600

    Added user routing and logout

--- both/components/Header/Header.jsx
+++ both/components/Header/Header.jsx
@@ -1,18 +1,44 @@
 Header = React.createClass({
+    getInitialState() {
+        return {
+            isLoggedIn: User.isLoggedIn()
+        };
+    },
+    logout() {
+      Meteor.logout((er)=>{
+        if(er) {
+          Materialize.toast(er.reason, 4000);
+        } else {
+          this.setState({isLoggedIn: !this.state.isLoggedIn});
+          FlowRouter.go('/');
+        }
+      }.bind(this));
+    },
+
   render() {
     var navStyle = {
       backgroundColor: "#3f51b5",
       paddingLeft: "12px"
     };
+
+    // {this.logout} is passing the parent component's logout method to its child component
+    var navOptions = User.isLoggedIn() ? (<LoggedInNav logout={this.logout}/>) : (<LoggedOutNav/>);
+
+    // if(User.isLoggedIn) {
+    //     navOptions = (
+    //         <LoggedInNav logout={this.logout}/> // make sure to pass login() props
+    //     );
+    // } else {
+    //     navOptions = (
+    //         <LoggedOutNav/>
+    //     );
+    // }
+
     return (
       <nav style={navStyle}>
         <div className="nav-wrapper">
-          <a href="/" className="brand-logo">App Name Here</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/">Home</a></li>
-            <li><a href="/register">Register</a></li>
-            <li><a href="/login">Login</a></li>            
-          </ul>
+          <a href="/" className="brand-logo">Application</a>
+          {navOptions}
         </div>
       </nav>
     );
